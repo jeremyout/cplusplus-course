9ac4a2077e2e38b266ba8162bfdfd960d4904b39
chewy927 <jeremyoutinen@gmail.com>
2021-10-04 18:19:08 -0500

    Declaring and initializing values

---
+++ Section6/InitializingVariables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// This program will calculate the area of a room in sqaure feet
+
+int main()
+{
+
+    int roomWidth { 0 };
+    int roomLength { 0 };
+
+    std::cout << "Enter the width of the room: ";
+    std::cin >> roomWidth;
+    std::cout << "Enter the length of the room: ";
+    std::cin >> roomLength;
+    
+    std::cout << "The area of the room is: " << roomWidth*roomLength << " square feet" << std::endl;
+
+    return 0;
+}
